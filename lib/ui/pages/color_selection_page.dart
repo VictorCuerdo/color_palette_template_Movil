a4eac86d68ef22a28135b5191eda18b66677c5c5
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2023-02-08 19:23:04 -0500

    allBaseColorsUpdated

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -17,26 +17,65 @@ class ColorSelectionPage extends StatelessWidget {
     ));
   }
 
-  @Override 
-  Widget build(BuildContext context){
-    return Scaffold(appBar: AppBar(title:const Text('Color palette app'),
-    actions: [
-      IconButton(onPressed: showDialog, icon: Icon(Icons.info_outline))
-    ],),
-    floatingActionButton: FloatingActionButton(onPressed:() => print('Hello'),
-    child: const Icon(Icons.plus_one),
-    ),
-    body: SafeArea(child: SingleChildScrollView(child: Column(mainAxisAlignment: MainAxisAlignment.start, children:[
-      ColorPalette(baseColor: '222831', baseColor2: '393E46', baseColor3: '00ADB5', baseColor4: 'EEEEEE', callback: showColor),
-      ColorPalette(baseColor: '222831', baseColor2: '393E46', baseColor3: '00ADB5', baseColor4: 'EEEEEE', callback: showColor),
-      ColorPalette(baseColor: '222831', baseColor2: '393E46', baseColor3: '00ADB5', baseColor4: 'EEEEEE', callback: showColor),
-      ColorPalette(baseColor: '222831', baseColor2: '393E46', baseColor3: '00ADB5', baseColor4: 'EEEEEE', callback: showColor),
-      ColorPalette(baseColor: '222831', baseColor2: '393E46', baseColor3: '00ADB5', baseColor4: 'EEEEEE', callback: showColor),
-      ColorPalette(baseColor: '222831', baseColor2: '393E46', baseColor3: '00ADB5', baseColor4: 'EEEEEE', callback: showColor),
-    
-    
-    
-    ])),))
+  @Override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Color palette app'),
+          actions: [
+            IconButton(onPressed: showDialog, icon: Icon(Icons.info_outline))
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('Hello'),
+          child: const Icon(Icons.plus_one),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ColorPalette(
+                    baseColor: '222831',
+                    baseColor2: '393E46',
+                    baseColor3: '00ADB5',
+                    baseColor4: 'EEEEEE',
+                    callback: showColor),
+                ColorPalette(
+                    baseColor: '5F7161',
+                    baseColor2: '6D8B74',
+                    baseColor3: 'EFEAD8',
+                    baseColor4: 'D0C9C0',
+                    callback: showColor),
+                ColorPalette(
+                    baseColor: 'F9CEEE',
+                    baseColor2: 'F9F3EE',
+                    baseColor3: 'CCF3EE',
+                    baseColor4: '97C4B8',
+                    callback: showColor),
+                ColorPalette(
+                    baseColor: '541690',
+                    baseColor2: 'FF4949',
+                    baseColor3: 'FF8D29',
+                    baseColor4: 'EFCD38',
+                    callback: showColor),
+                ColorPalette(
+                    baseColor: '764AF1',
+                    baseColor2: '9772FB',
+                    baseColor3: 'F2F2F2',
+                    baseColor4: 'F32424',
+                    callback: showColor),
+                ColorPalette(
+                    baseColor: 'E8F9FD',
+                    baseColor2: '99C4C8',
+                    baseColor3: 'E5CB9F',
+                    baseColor4: 'EEE4A8',
+                    callback: showColor),
+              ],
+            ),
+          ),
+        ));
   }
 
+  void showColor(String value) {}
 }
